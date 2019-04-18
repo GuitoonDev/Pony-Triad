7418409021d8188683ed8b2d212ccad7702d735e
as_gvergeot <gaetan.vergeot@altshift.fr>
2019-04-18 14:24:21 +0200

    - Adding omnisharp + .vscode - Test update in editor mode script

---
+++ Assets/Scripts/TestEditorInUpdate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class TestEditorInUpdate : MonoBehaviour
+{
+    public int testValue = 0;
+
+    private void Update() {
+        testValue++;
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.QueuePlayerLoopUpdate();
+#endif
+    }
+}
