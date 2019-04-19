03c60b998f81bfb49fd425549229e218d5d273c4
as_gvergeot <gaetan.vergeot@altshift.fr>
2019-04-19 14:24:51 +0200

    - Move TextMeshPro in _Plugin folder - Create Card prefab

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro powerUpText;
+    [SerializeField] private TextMeshPro powerDownText;
+    [SerializeField] private TextMeshPro powerLeftText;
+    [SerializeField] private TextMeshPro powerRightText;
+
+    [SerializeField] private SpriteRenderer cardImage;
+
+}
---
+++ Assets/Scripts/SelectableArea.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class SelectableArea : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer borderline;
+}
