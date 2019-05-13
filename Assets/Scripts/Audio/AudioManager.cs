7bf22464044c4e4af9aae6b1851434483885ec62
as_gvergeot <gaetan.vergeot@altshift.fr>
2019-05-13 19:25:59 +0200

    - Update AudioManager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,31 +1,21 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace Audio
 {
-    [RequireComponent(typeof(AudioSource))]
     public class AudioManager : MonoBehaviour
     {
         public static AudioManager Instance { get; set; }
+
+        [Header("Tracks")]
+        [SerializeField] private AudioSource sfxTrack = null;
+        [SerializeField] private AudioSource musicTrack = null;
 
         [Header("Playable Musics")]
         [SerializeField] private PlayableMusic gameMusic = null;
         [SerializeField] private PlayableMusic victoryMusic = null;
 
-        private AudioSource musicAudioSource = null;
-        private AudioSource MusicAudioSource {
-            get {
-                if (musicAudioSource == null) {
-                    musicAudioSource = GetComponent<AudioSource>();
-                }
-
-                return musicAudioSource;
-            }
-        }
-
         private PlayableMusic currentMusic = null;
-
-        private Coroutine waitForEndIntroMusicCoroutine = null;
 
         private void Awake() {
             if (Instance == null) {
@@ -48,29 +38,24 @@ namespace Audio
 
         private void PlayMusic(PlayableMusic _playableMusic) {
             if (currentMusic != _playableMusic) {
-                MusicAudioSource.Stop();
+                musicTrack.Stop();
 
                 currentMusic = _playableMusic;
 
-                MusicAudioSource.clip = currentMusic.LoopMusic;
+                musicTrack.clip = currentMusic.LoopMusic;
 
                 if (currentMusic.IntroMusic != null) {
-                    MusicAudioSource.PlayOneShot(currentMusic.IntroMusic);
-                    MusicAudioSource.PlayScheduled(AudioSettings.dspTime + currentMusic.IntroMusic.length);
+                    musicTrack.PlayOneShot(currentMusic.IntroMusic);
+                    musicTrack.PlayScheduled(AudioSettings.dspTime + currentMusic.IntroMusic.length);
                 }
                 else if (currentMusic.LoopMusic != null) {
-                    MusicAudioSource.Play();
+                    musicTrack.Play();
                 }
             }
-
         }
 
         public void PlaySound(AudioClip _audioClip) {
-            // TODO Faire un système de pistes de SFX !!!
-            AudioSource.PlayClipAtPoint(_audioClip, Camera.main.transform.position);
-
-            // SoundEffectPlayer newSoundEffect = Instantiate(soundEffectPlayerPrefab, transform, false);
-            // newSoundEffect.PlayAudioClip(_audioClip);
+            sfxTrack.PlayOneShot(_audioClip);
         }
     }
 }
