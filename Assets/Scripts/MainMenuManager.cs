24dea4bff658d7797843d7df1ca898d16a6d2b1c
gvergeot <vergeot.gaetan@gmail.com>
2019-05-26 18:45:19 +0200

    - Manage new Fonts - Create a main menu screen

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName;
+
+    public void LaunchGame() {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
