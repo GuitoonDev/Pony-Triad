37e347fa48f365e9031c953a9be83c2072874d7e
gvergeot <vergeot.gaetan@gmail.com>
2019-06-05 20:34:38 +0200

    - Rename CardList variable

--- Assets/Scripts/Data/CardList.cs
+++ Assets/Scripts/Data/CardList.cs
@@ -3,20 +3,19 @@
 [CreateAssetMenu(fileName = "CardList", menuName = "ProtoTriad/CardList")]
 public class CardList : ScriptableObject
 {
-    [SerializeField]
-    private CardData[] cardDatasArray = null;
+    [SerializeField] private CardData[] cardDataArray = null;
 
     public CardData GetRandomCard() {
-        int randomCardIndex = Random.Range(0, cardDatasArray.Length);
-        return cardDatasArray[randomCardIndex];
+        int randomCardIndex = Random.Range(0, cardDataArray.Length);
+        return cardDataArray[randomCardIndex];
     }
 
     public CardData[] GetRandomCard(int _cardToDrawNumber) {
         CardData[] cardsPicked = new CardData[_cardToDrawNumber];
 
         for (int i = 0; i < cardsPicked.Length; i++) {
-            int randomCardIndex = Random.Range(0, cardDatasArray.Length);
-            cardsPicked[i] = cardDatasArray[randomCardIndex];
+            int randomCardIndex = Random.Range(0, cardDataArray.Length);
+            cardsPicked[i] = cardDataArray[randomCardIndex];
         }
 
         return cardsPicked;
