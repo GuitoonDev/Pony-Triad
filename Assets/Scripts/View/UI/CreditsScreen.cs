8ac47ed87e1b12f6257aa74225d8a516fa2765f4
gvergeot <vergeot.gaetan@gmail.com>
2019-06-30 16:48:32 +0200

    - Update Graphic Settings - Remove TestInEditorUpdate script

--- Assets/Scripts/View/UI/CreditsScreen.cs
+++ Assets/Scripts/View/UI/CreditsScreen.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 
 public class CreditsScreen : MonoBehaviour
 {
-    [SerializeField] private Image panel;
+    [SerializeField] private Image panel = null;
 
     public void Show() {
         panel.gameObject.SetActive(true);
